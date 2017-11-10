9b01041982e77c599a128c8fedf957ba243d0f36
GeoEn <GeoEn@Klien>
2017-11-10 14:13:05 -0700

    Added TestDie junit test case

---
+++ test/com/esof322/pa2/TestDie.java
@@ -0,0 +1,24 @@
+package com.esof322.pa2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDie {
+
+	@Test
+	//Tests to make sure the Dice are rolling an integer between 1 and 6
+	public void testDie() {
+		Die die1 = new Die();
+		Die die2 = new Die();
+		die1.rollDie();
+		die2.rollDie();
+		assertTrue(1 <= die1.getValue() && die1.getValue() <= 6);
+		assertTrue(1 <= die2.getValue() && die2.getValue() <= 6);
+	}
+
+}
